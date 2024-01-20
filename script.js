8a413c6b66d19591ad79aea2ba3114c88c09613e
praveen <praveen.mangal@shipway.com>
2024-01-20 21:38:41 +0530

    Created basic calculator function logic

---
+++ script.js
@@ -0,0 +1,26 @@
+function add(a, b) {
+	return a + b;
+}
+function subtract(a, b) {
+	return a - b;
+}
+function multiply(a, b) {
+	return a * b;
+}
+function divide(a, b) {
+	return a / b;
+}
+
+function operate(operator, num1, num2) {
+	if (operator == '+') {
+		return add(num1, num2);
+	} else if (operator == '-') {
+		return subtract(num1, num2);
+	} else if (operator == '*') {
+		return multiply(num1, num2);
+	} else if (operator == '/') {
+		return divide(num1, num2);
+	}
+}
+
+let num1, op, num2;
